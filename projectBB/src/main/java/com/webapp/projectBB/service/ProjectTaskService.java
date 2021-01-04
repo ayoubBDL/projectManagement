df9d3335f873867930d400500c76647c6b5ed992
ayoubBDL <51295281+ayoubBDL@users.noreply.github.com>
2021-01-04 16:08:07 +0100

    project management

---
+++ projectBB/src/main/java/com/webapp/projectBB/service/ProjectTaskService.java
@@ -0,0 +1,35 @@
+package com.webapp.projectBB.service;
+
+import com.webapp.projectBB.domain.ProjectTask;
+import com.webapp.projectBB.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdatePT(ProjectTask projectTask){
+
+        if(projectTask.getStatus()==null || projectTask.getStatus()==""){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+
+    public Iterable<ProjectTask> findAll(){
+        return projectTaskRepository.findAll();
+    }
+
+    public ProjectTask findById(Long id){
+        return projectTaskRepository.getById(id);
+    }
+
+    public void delete(Long id){
+        ProjectTask projectTask = findById(id);
+        projectTaskRepository.delete(projectTask);
+    }
+}
---
+++ projectBB/src/test/java/com/webapp/projectBB/ProjectBbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.projectBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
